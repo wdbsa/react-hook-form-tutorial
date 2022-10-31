a0fc9b225908859e97fd8faedcfb412baa96ac0b
Mark Abuyabor <mark@zeguestlist.com>
2022-10-31 23:56:35 +0800

    adding the tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import './App.css';
+import Navbar from './Components/Navbar';
 
 function App() {
   return (
     <div className="App">
-    
+        <Navbar />
     </div>
   );
 }
